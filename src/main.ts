5751dc67ffd2b2ce4fb7fbf33b5bbf066a45b063
Vorrapong Kertnat <47938031+iceman951@users.noreply.github.com>
2025-07-18 18:00:12 +0000

    poc: supabase Auth

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,15 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import PrimeVue from 'primevue/config';
+import Aura from '@primeuix/themes/aura';
+import ToastService from 'primevue/toastservice';
 
-createApp(App).mount('#app')
+createApp(App)
+    .use(PrimeVue, {
+        theme: {
+            preset: Aura
+        }
+    })
+    .use(ToastService)
+    .mount('#app')
